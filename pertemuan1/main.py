ce7779dbcc8fb477a946ed0925b36e2ab806c2ac
nelann <93913350+Nelann@users.noreply.github.com>
2022-11-01 07:08:02 +0000

    add: new feature

--- pertemuan1/main.py
+++ pertemuan1/main.py
@@ -1,1 +1,18 @@
-print("Hello, World")
+employees = [
+  {
+    "id": 1,
+    "name": "John",
+  },
+  {
+    "id": 2,
+    "name": "Budi",
+  },
+  {
+    "id": 3,
+    "name": "Joko",
+  },
+]
+
+for employee in employees:
+  if (employee['id'] == 3):
+    print(f"Hello my name is {employee['name']}")
